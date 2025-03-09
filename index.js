578d8ee7aee2a889d5fc342d4a09ced618b2a8bb
Fred <ftanon@student.42.fr>
2025-03-09 21:31:40 +0800

    game draft amended

--- index.js
+++ index.js
@@ -20,39 +20,24 @@ fastify.register(fastifyStatic, {
 
 fastify.register(fastifyWebsocket)
 
+let gameStart = false;
 let players = [];
-
 let gameState = {
   type: "update",
   paddles: [ { y: 10, playerID: 0, side: "left" }, { y: 10, playerID: 0, side: "right" } ],
   ball: { x: 400, y: 200, vx: 4, vy: 4 },
-  scores: {left: 1, right: 0}
+  scores: {left: 0, right: 0}
 };
+
+setInterval(updateGame, 30);
 
 fastify.register(async (fastify) => {
     fastify.get("/ws", { websocket: true }, (connection, req) => {
-        setInterval(() => {
-            gameState.ball.x += gameState.ball.vx;
-            gameState.ball.y += gameState.ball.vy;
-            if (gameState.ball.y <= 0 || gameState.ball.y >= 400)
-                gameState.ball.vy *= -1;
-            if (gameState.ball.x <= 0) 
-            {
-                gameState.scores.right += 1;
-                resetBall();
-            } 
-            else if (gameState.ball.x >= 800)
-            {
-                gameState.scores.left++;
-                resetBall();
-            }
-            if (gameState.ball.x <= 20 && gameState.ball.y >= gameState.paddles[0].y && gameState.ball.y <= gameState.paddles[0].y + 80)
-                gameState.ball.vx *= -1;
-            if (gameState.ball.x >= 780 && gameState.ball.y >= gameState.paddles[1].y && gameState.ball.y <= gameState.paddles[1].y + 80)
-                gameState.ball.vx *= -1;
-            connection.send(JSON.stringify(gameState));
-        }, 30);
-
+    
+    gameStart = true;
+    const playerIndex = players.length;
+    players.push(connection);
+    
     connection.on("message", (message) =>
     {
         const data = JSON.parse(message);
@@ -65,18 +50,47 @@ fastify.register(async (fastify) => {
         else if (data === "l")
           gameState.paddles[1].y += 5;
     });
-
-      // socket.on('close', function ()
-      // {
-      //     console.log('S: Client closed connection');
-      // });
+    connection.on('close', () =>
+    {
+      players.splice(playerIndex, 1)
+    });
   })
 })
+
+function updateGame()
+{
+    if (gameStart)
+    {
+        gameState.ball.x += gameState.ball.vx;
+        gameState.ball.y += gameState.ball.vy;
+        if (gameState.ball.y <= 0 || gameState.ball.y >= 400)
+            gameState.ball.vy *= -1;
+        if (gameState.ball.x <= 0) 
+        {
+            gameState.scores.right += 1;
+            resetBall();
+        } 
+        else if (gameState.ball.x >= 800)
+        {
+            gameState.scores.left++;
+            resetBall();
+        }
+        if (gameState.ball.x <= 20 && gameState.ball.y >= gameState.paddles[0].y && gameState.ball.y <= gameState.paddles[0].y + 80)
+            gameState.ball.vx *= -1;
+        if (gameState.ball.x >= 780 && gameState.ball.y >= gameState.paddles[1].y && gameState.ball.y <= gameState.paddles[1].y + 80)
+            gameState.ball.vx *= -1;
+        broadcastState();
+    }
+}
 
 function resetBall() {
   gameState.ball = { x: 400, y: 200, vx: 4, vy: 4 };
 }
 
+function broadcastState()
+{
+    players.forEach(player => player.send(JSON.stringify(gameState)));
+}
 
 try {
   await fastify.listen({ port: 3000, host: '0.0.0.0'})
