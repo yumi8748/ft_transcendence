d61a5405efc83219f3d3d46a69247621013a4f03
Frederic Tanon <ftanon@bess-f4r5s1.clusters.42paris.fr>
2025-04-17 15:31:50 +0200

    plug to main

--- public/displayTournament.js
+++
@@ -1,8 +0,0 @@
-var contentDiv = document.getElementById('content');
-
-function displayTournament()
-{
-    contentDiv.innerHTML = `<h2>tournament page</h2>`;
-}
-
-export default displayTournament;
